dc6e707f38952953640fb4ac52bebfe56b1d17fc
DatTran <trancongdat.datct@gmail.com>
2023-10-30 15:54:35 +0700

    first commit

---
+++ src/main/java/com/example/springbootpratices/SpringBootPraticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpratices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPraticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPraticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootpratices/SpringBootPraticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpratices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPraticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
