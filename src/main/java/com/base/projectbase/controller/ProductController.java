44d8cba5bfb35761306fc147d36139727e52e57a
Dat Tran <khiemtran21012000@gmail.com>
2024-04-02 16:40:47 +0700

    add logging & optimize import

--- src/main/java/com/base/projectbase/controller/ProductController.java
+++ src/main/java/com/base/projectbase/controller/ProductController.java
@@ -2,15 +2,23 @@ package com.base.projectbase.controller;
 
 import com.base.projectbase.dto.ProductDTO;
 import com.base.projectbase.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.yaml.snakeyaml.events.Event;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -23,12 +31,14 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<ProductDTO>> getAllProducts() {
         List<ProductDTO> productDTOs = productService.getAllProducts();
+        log.info("Get list of Products with size: " + productDTOs.size());
         return ResponseEntity.ok(productDTOs);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) {
         ProductDTO productDTO = productService.getProduct(id);
+        log.info("Get a product with name: " + productDTO.getProductName());
         return ResponseEntity.ok(productDTO);
     }
 
@@ -36,18 +46,21 @@ public class ProductController {
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
         ProductDTO currentProduct = productService.getProduct(id);
         ProductDTO updatedProduct = productService.updateProduct(currentProduct, id);
+        log.info("Updated a product with name: " + updatedProduct.getProductName());
         return ResponseEntity.ok(updatedProduct);
     }
 
     @PostMapping
     public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) {
         ProductDTO createdProduct = productService.createProduct(productDTO);
+        log.info("Created a product with name: " + createdProduct.getProductName());
         return ResponseEntity.ok(createdProduct);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
+        log.info("Deleted a product with ID: " + id);
         return ResponseEntity.ok(String.format("Deleted Product with ID %d", id));
     }
 }
