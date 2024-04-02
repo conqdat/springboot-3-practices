850cdb662eaaa891da783fb3e383f87dc230b41d
DatTran <trancongdat.datct@gmail.com>
2024-04-02 11:35:45 +0700

    fix id import

--- src/main/java/com/base/projectbase/model/Product.java
+++ src/main/java/com/base/projectbase/model/Product.java
@@ -1,18 +1,19 @@
 package com.base.projectbase.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Product {
 
-    @Id
+    @jakarta.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
