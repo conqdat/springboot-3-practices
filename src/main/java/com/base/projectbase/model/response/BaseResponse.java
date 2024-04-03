4e26852213514eb1fafc642acd9cc78ded280a3e
Dat Tran <khiemtran21012000@gmail.com>
2024-04-03 16:08:50 +0700

    convert to records

--- src/main/java/com/base/projectbase/model/response/BaseResponse.java
+++ src/main/java/com/base/projectbase/model/response/BaseResponse.java
@@ -1,14 +1,4 @@
 package com.base.projectbase.model.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class BaseResponse {
-    private int status;
-    private String message;
-    private Object data;
+public record BaseResponse(int status, String message, Object data) {
 }
--- src/main/java/com/base/projectbase/model/response/ProductResponse.java
+++ src/main/java/com/base/projectbase/model/response/ProductResponse.java
@@ -1,19 +1,6 @@
 package com.base.projectbase.model.response;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-@Setter
-public class ProductResponse<T> {
-    private HttpStatus status;
-    private String message;
-    private T data;
-
-    public ProductResponse(HttpStatus status, String message, T data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
+public record ProductResponse<T>(HttpStatus status, String message, T data) {
 }
